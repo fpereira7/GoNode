54cdfffb753451dcf14b2be1d9bd4138b5a028ce
Francisco Pereira <francisco.anpereira@gmail.com>
2019-05-15 21:13:50 -0300

    add and configure nunjucks for templates

--- modulo1/index.js
+++ modulo1/index.js
@@ -1,27 +1,20 @@
-const express = require('express');
+const express = require("express");
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  console.log(
-      `Host: ${req.headers.host} | URL: ${req.url} | method: ${req.method}`
-  );  
+nunjucks.configure('views', {
+   autoescape: true,
+   express: app,
+   watch: true,
 
-req.appName = 'GoNode'; 
-
-  return next();
-};
-
-app.use(logMiddleware);
-
-app.get('/', logMiddleware, (req, res) =>{
-    return res.send(`Bem vindo ao ${req.appName}, ${req.query.name}`);
 });
 
-app.get('/nome/:name', (req, res) => {
-    return res.json({
-        message: `welcome, ${req.params.name}`
-    });
+app.set('view engine', 'njk');
+
+
+app.get('/', (req, res) =>{
+    return res.render("list", { name: "Diego" });
 });
 
 app.listen(3000);
